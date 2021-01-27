e4a9e2bbeeaee649565497bc1d180e880d8f9845
alex240590 <lelek1@bk.ru>
2021-01-27 22:26:37 +0300

    [Lesson 1] ver.0.1

---
+++ lesson1-user/geekspring/src/main/java/ru/geekbrains/security/SecurityConfig.java
@@ -0,0 +1,81 @@
+package ru.geekbrains.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    public void authConfigure(AuthenticationManagerBuilder auth,
+                              UserAuthService userAuthService,
+                              PasswordEncoder passwordEncoder) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN");
+
+        auth.inMemoryAuthentication()
+                .withUser("user2")
+                .password(passwordEncoder.encode("password"))
+                .roles("GUEST");
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userAuthService);
+        provider.setPasswordEncoder(passwordEncoder);
+
+        auth.authenticationProvider(provider);
+    }
+
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/api/**")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .hasAnyRole("ADMIN", "GUEST")
+                    .and()
+                    .httpBasic()
+                    .authenticationEntryPoint((req, resp, exception) -> {
+                        resp.setContentType("application/json");
+                        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        resp.setCharacterEncoding("UTF-8");
+                        resp.getWriter().println("{ \"error\": \"" + exception.getMessage() + "\" }");
+                    })
+                    .and()
+                    .csrf().disable()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    public static class UiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/*.css", "/*.js").anonymous()
+                    .antMatchers("/user/**").hasRole("ADMIN")
+                    .anyRequest().authenticated() // .anonymous() для неавторизованного доступа
+                    .and()
+                    .formLogin();
+        }
+    }
+}
